dda253c499c32042c240bbd95b222418c64125cc
0x323143 <toctoc21c@gmail.com>
2024-05-17 16:37:54 +0900

    Update echo-server.js

--- echo-server.js
+++ echo-server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
+    console.log(request.headers);
     response.writeHead(200, {
         'Content-Type': 'text/event-stream',
         'tESt': '1111'
